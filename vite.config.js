a6a89e78352266657b3002650dab399923b32c4e
Rishabhjaiswal2006 <ajamanjaiz14@gmail.com>
2025-08-18 01:26:42 +0530

    updated-repo-name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/landing_page_react/',
+  base: '/FARM-RE/',
   build: {
     outDir: 'dist',
   },
